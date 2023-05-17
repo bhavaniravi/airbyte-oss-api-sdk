b9f597a081d0c6e4f82b07248d70814ba92f7f6b
bhavaniravi <bhava0895@gmail.com>
2023-05-17 14:05:09 +0530

    add delete and get sources API

--- api/sources.py
+++ api/sources.py
@@ -39,7 +39,6 @@ class SourceDefinitionAPI(AirbyteHook):
         )
 
         if response.status_code == 200:
-            print(response.json())
             return SourceDefinition.from_dict(response.json())
 
 
@@ -52,6 +51,49 @@ class SourcesAPI(AirbyteHook):
             json={"sourceId": source_id, "connectionId": connection_id},
         )
 
+    def check_source_connection(
+        self,
+        workspace_id,
+        source_name,
+        source_definition_id,
+        params,
+        validate_params=False,
+    ):
+        """_summary_
+
+        Args:
+            workspace_id (_type_): _description_
+            source_name (_type_): _description_
+            source_definition_id (_type_): _description_
+            params (_type_): _description_
+            validate_params (bool, optional): set validate params if used from outside of create_source
+
+        Returns:
+            _type_: _description_
+        """
+        if validate_params:
+            source_def = SourceDefinitionAPI(self.connection)
+            source_def = source_def.get_source_definition(
+                source_definition_id=source_definition_id
+            )
+            source_type = source_def.name.lower()
+            params["source_type"] = source_def
+            SourceClass = getattr(shared, f"Source{source_type.title()}")
+            source = SourceClass(**params)
+            params = dataclasses.asdict(source)
+
+        return self.run(
+            method="POST",
+            endpoint=f"api/{self.connection.api_version}/sources/check_connection",
+            headers=self.headers,
+            json={
+                "workspaceId": workspace_id,
+                "name": source_name,
+                "sourceDefinitionId": source_definition_id,
+                "connectionConfiguration": params,
+            },
+        )
+
     def create_source(self, workspace_id, source_name, source_definition_id, params):
         source_def = SourceDefinitionAPI(self.connection)
         source_def = source_def.get_source_definition(
@@ -61,7 +103,10 @@ class SourcesAPI(AirbyteHook):
         params["source_type"] = source_def
         SourceClass = getattr(shared, f"Source{source_type.title()}")
         source = SourceClass(**params)
-
+        params = dataclasses.asdict(source)
+        self.check_source_connection(
+            workspace_id, source_name, source_definition_id, params
+        )
         return self.run(
             method="POST",
             endpoint=f"api/{self.connection.api_version}/sources/create",
@@ -70,9 +115,25 @@ class SourcesAPI(AirbyteHook):
                 "workspaceId": workspace_id,
                 "name": source_name,
                 "sourceDefinitionId": source_definition_id,
-                "connectionConfiguration": dataclasses.asdict(source),
+                "connectionConfiguration": params,
             },
         )
 
     def get_source(self, source_id):
-        pass
+        # there is a bug in this API, the source_id is returned even after deletion
+        return self.run(
+            method="POST",
+            endpoint=f"api/{self.connection.api_version}/sources/get",
+            headers=self.headers,
+            json={"sourceId": source_id},
+        )
+
+    def delete_source(self, source_id):
+        response = self.run(
+            method="POST",
+            endpoint=f"api/{self.connection.api_version}/sources/delete",
+            headers=self.headers,
+            json={"sourceId": source_id},
+        )
+        print(response.text)
+        return response
--- tests/test_source.py
+++ tests/test_source.py
@@ -2,11 +2,10 @@ from api.sources import SourcesAPI, SourceDefinitionAPI
 import pytest
 
 workspace_id = "2d51cc69-400c-4c65-9409-b52def56235f"
-connection_id = "c5bc8a5d-c9c4-4399-89f4-1d5dd34ef170"
-source_id = "cc9f8665-7953-451f-8679-289ac5b9aefa"
+# connection_id = "c5bc8a5d-c9c4-4399-89f4-1d5dd34ef170"
+# source_id = "cc9f8665-7953-451f-8679-289ac5b9aefa"
 source_definition_id = "decd338e-5647-4c0b-adf4-da0e75f5a750"  # for postgres
-# pytestmark = pytest.mark.usefixtures('connection')
-
+source_id = "6875991a-f77a-4cd2-a537-18d045c5c52a"
 
 # def test_source_definition(connection):
 #     hook = SourceDefinitionAPI(connection)
@@ -24,23 +23,35 @@ source_definition_id = "decd338e-5647-4c0b-adf4-da0e75f5a750"  # for postgres
 #     response = hook.list_sources_definitions(workspace_id=workspace_id)
 
 
-def test_create_source(connection):
+# def test_create_source(connection):
+#     hook = SourcesAPI(connection)
+#     response = hook.create_source(
+#         source_name="new_pg",
+#         workspace_id=workspace_id,
+#         source_definition_id="decd338e-5647-4c0b-adf4-da0e75f5a750",
+#         params={
+#             "host": "localhost",
+#             "port": 5432,
+#             "username": "postgres",
+#             "password": "password",
+#             "database": "postgres",
+#             "schemas": ["public"],
+#             "ssl_mode": {"mode": "disable"},
+#             "tunnel_method": {"tunnel_method": "NO_TUNNEL"},
+#             "replication_method": {"method": "Standard"},
+#             "jdbc_url_params": "",
+#         },
+#     )
+
+
+def test_get_source(connection):
+    # there is a bug in this API, the source_id is returned even after deletion
     hook = SourcesAPI(connection)
-    response = hook.create_source(
-        source_name="new_pg",
-        workspace_id=workspace_id,
-        source_definition_id="decd338e-5647-4c0b-adf4-da0e75f5a750",
-        params={
-            "host": "localhost",
-            "port": 5432,
-            "username": "postgres",
-            "password": "password",
-            "database": "postgres",
-            "schemas": ["public"],
-            "ssl_mode": {"mode": "disable"},
-            "tunnel_method": {"tunnel_method": "NO_TUNNEL"},
-            "replication_method": {"method": "Standard"},
-            "jdbc_url_params": "",
-        },
-    )
+    response = hook.get_source(source_id=source_id)
     print(response.json())
+
+
+def test_delete_source(connection):
+    hook = SourcesAPI(connection)
+    response = hook.delete_source(source_id=source_id)
+    print("------------------", "source_id", source_id)
